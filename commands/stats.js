543ecc72b2ef6efa2eb164b95e111c3c4ffc61c9
AlphaKretin <deewhydeeecks@gmail.com>
2019-01-15 15:58:38 +1030

    Update stats command to include embed

--- commands/stats.js
+++ commands/stats.js
@@ -1,12 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const bot_1 = require("../modules/bot");
 const cardSearch_1 = require("../modules/cardSearch");
 const Command_1 = require("../modules/Command");
 const data_1 = require("../modules/data");
 const util_1 = require("../modules/util");
 const names = ["stats", "notext"];
-const func = async (msg) => {
+const func = async (msg, mobile) => {
     const langs = util_1.getLang(msg);
     try {
         const card = await data_1.data.getCard(langs.msg, langs.lang1);
@@ -14,10 +13,22 @@ const func = async (msg) => {
             const stats = await cardSearch_1.generateCardStats(card, langs.lang2, msg);
             const codes = await card.aliasIDs;
             const codeString = codes.join(" | ");
-            await bot_1.bot.createMessage(msg.channel.id, "__**" + card.text[langs.lang2].name + "**__:\n" + "**ID**: " + codeString + "\n" + stats);
+            if (mobile) {
+                await msg.channel.createMessage("__**" + card.text[langs.lang2].name + "**__:\n" + "**ID**: " + codeString + "\n" + stats);
+            }
+            else {
+                await msg.channel.createMessage({
+                    embed: {
+                        description: stats,
+                        footer: { text: codeString },
+                        thumbnail: { url: card.imageLink },
+                        title: card.text[langs.lang2].name
+                    }
+                });
+            }
         }
         else {
-            await bot_1.bot.createMessage(msg.channel.id, "Sorry, I can't find a card for `" + langs.msg + "`!");
+            await msg.channel.createMessage("Sorry, I can't find a card for `" + langs.msg + "`!");
         }
     }
     catch (e) {
--- commands/stats.ts
+++ commands/stats.ts
@@ -1,12 +1,11 @@
 import * as Eris from "eris";
-import { bot } from "../modules/bot";
 import { generateCardStats } from "../modules/cardSearch";
 import { Command } from "../modules/Command";
 import { data } from "../modules/data";
 import { getLang } from "../modules/util";
 
 const names = ["stats", "notext"];
-const func = async (msg: Eris.Message): Promise<void> => {
+const func = async (msg: Eris.Message, mobile: boolean): Promise<void> => {
     const langs = getLang(msg);
     try {
         const card = await data.getCard(langs.msg, langs.lang1);
@@ -14,12 +13,22 @@ const func = async (msg: Eris.Message): Promise<void> => {
             const stats = await generateCardStats(card, langs.lang2, msg);
             const codes = await card.aliasIDs;
             const codeString = codes.join(" | ");
-            await bot.createMessage(
-                msg.channel.id,
-                "__**" + card.text[langs.lang2].name + "**__:\n" + "**ID**: " + codeString + "\n" + stats
-            );
+            if (mobile) {
+                await msg.channel.createMessage(
+                    "__**" + card.text[langs.lang2].name + "**__:\n" + "**ID**: " + codeString + "\n" + stats
+                );
+            } else {
+                await msg.channel.createMessage({
+                    embed: {
+                        description: stats,
+                        footer: { text: codeString },
+                        thumbnail: { url: card.imageLink },
+                        title: card.text[langs.lang2].name
+                    }
+                });
+            }
         } else {
-            await bot.createMessage(msg.channel.id, "Sorry, I can't find a card for `" + langs.msg + "`!");
+            await msg.channel.createMessage("Sorry, I can't find a card for `" + langs.msg + "`!");
         }
     } catch (e) {
         throw e;
