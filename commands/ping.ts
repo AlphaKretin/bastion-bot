6e1012075768936db51657a113fde2d42ced7e53
AlphaKretin <deewhydeeecks@gmail.com>
2019-01-19 15:44:39 +1030

    Update Ping command

--- commands/ping.ts
+++ commands/ping.ts
@@ -4,7 +4,10 @@ import { Command } from "../modules/Command";
 
 const names = ["ping"];
 const func = async (msg: Eris.Message): Promise<void> => {
-    await bot.createMessage(msg.channel.id, "Pong!");
+    const time = Date.now();
+    const mes = await msg.channel.createMessage("Pong!");
+    const ping = new Date(Date.now() - time);
+    await mes.edit("Pong! (" + ping.getMilliseconds() + " ms)");
 };
 
 export const cmd = new Command(names, func);
