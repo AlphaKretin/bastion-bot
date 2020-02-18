b775b10e4ee275d14ed18ca6ed4be1d51dcd6591
AlphaKretin <deewhydeeecks@gmail.com>
2020-02-18 15:33:56 +1030

    Add periodic auto-update

--- modules/data.ts
+++ modules/data.ts
@@ -4,3 +4,9 @@ import { YgoData } from "ygopro-data";
 const dataOpts = JSON.parse(fs.readFileSync("config/dataOpts.json", "utf8"));
 export const data = new YgoData("config/dataOpts.json", "dbs/");
 export const imageExt = dataOpts.imageExt;
+
+const autoUpdateInterval = 1000 * 60 * 60 * 24; //1k ms/s * 60 s/m * 60m/h * 24h/day
+setInterval(() => {
+	console.log("Starting auto-update!");
+	data.update().catch(e => console.error(e));
+}, autoUpdateInterval);
