378d2cd22aa2d25b6f421926ccaa896569ef43f9
AlphaKretin <deewhydeeecks@gmail.com>
2020-02-18 23:39:51 +1030

    Add class-side support for getting top stats

--- modules/stats.ts
+++ modules/stats.ts
@@ -5,22 +5,22 @@ import { LangPayload } from "./util";
 
 const statsDbPath = __dirname + "/../stats/stats.db3";
 class Stats {
-    private db: Promise<sqlite.Database>;
-    constructor() {
-    	this.db = this.getDB();
-    }
+	private db: Promise<sqlite.Database>;
+	constructor() {
+		this.db = this.getDB();
+	}
 
-    private async getDB(): Promise<sqlite.Database> {
+	private async getDB(): Promise<sqlite.Database> {
     	const init = !(await fs.exists(statsDbPath));
     	const db = await sqlite.open(statsDbPath);
     	if (init) {
-    		const dump = await fs.readFile(__dirname + "/../stats/stats.sql", "utf8");
+    		const dump = await fs.readFile(__dirname + "/../stats/stats.db3.sql", "utf8");
     		await db.exec(dump);
     	}
     	return db;
-    }
+	}
 
-    public async writeSearch(msg: Eris.Message, userQuery: LangPayload, result: number, mobile: boolean): Promise<void> {
+	public async writeSearch(msg: Eris.Message, userQuery: LangPayload, result: number, mobile: boolean): Promise<void> {
     	const db = await this.db;
     	const statement = await db.prepare("INSERT INTO cards VALUES(?,?,?,?,?,?,?,?)");
     	const snowflake = msg.id;
@@ -31,9 +31,9 @@ class Stats {
     	const lang1 = userQuery.lang1;
     	const lang2 = userQuery.lang2;
     	await statement.run(snowflake, server, user, query, result, mobile, lang1, lang2);
-    }
+	}
 	
-    public async writeCommand(msg: Eris.Message, name: string): Promise<void> {
+	public async writeCommand(msg: Eris.Message, name: string): Promise<void> {
     	const db = await this.db;
     	const statement = await db.prepare("INSERT INTO commands VALUES(?,?,?,?,?)");
     	const snowflake = msg.id;
@@ -42,7 +42,21 @@ class Stats {
     	const server = chan instanceof Eris.GuildChannel ? chan.guild.id : user; //user === server indicates DM
     	const args = msg.content;
     	await statement.run(snowflake, server, user, name, args);
-    }
+	}
+	
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	public async topCards(): Promise<any[]> {
+		const db = await this.db;
+		return await db.all("SELECT result, COUNT(result) AS times "+ 
+			"FROM cards GROUP BY result ORDER BY times DESC");
+	}
+
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	public async topCommands(): Promise<any[]> {
+		const db = await this.db;
+		return await db.all("SELECT name, COUNT(name) AS times "+ 
+			"FROM commands GROUP BY name ORDER BY times DESC");
+	}
 }
 
 export const stats = new Stats();
