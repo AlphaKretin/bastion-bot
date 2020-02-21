0220a608da2ff8e74cf4c170c392308050e9fd8e
AlphaKretin <deewhydeeecks@gmail.com>
2020-02-21 17:10:09 +1030

    Add missing metrics

--- commands/metrics.js
+++ commands/metrics.js
@@ -2,10 +2,15 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const Command_1 = require("../modules/Command");
 const stats_1 = require("../modules/stats");
+const bot_1 = require("../modules/bot");
 const names = ["metric"];
 const func = async (msg) => {
     const { activeUsers, cardCount, commandCount, cardsPerUser, commandsPerUser } = await stats_1.stats.getMetrics();
+    const serverCount = bot_1.bot.guilds.size;
+    const userCount = bot_1.bot.users.size;
     return await msg.channel.createMessage("I've calculated the following metrics:\n"
+        + "I am in **" + serverCount + " servers**.\n"
+        + "**" + userCount + " users** share a server with me.\n"
         + "**" + activeUsers + " unique users** have searched for cards and/or used commands.\n"
         + "**" + cardCount + " cards** (non-unique) have been searched for.\n"
         + "Each active user has searched an average of **" + cardsPerUser.toFixed(2) + " cards** each.\n"
--- commands/metrics.ts
+++ commands/metrics.ts
@@ -1,11 +1,16 @@
 import * as Eris from "eris";
 import { Command } from "../modules/Command";
 import { stats } from "../modules/stats";
+import { bot } from "../modules/bot";
 
 const names = ["metric"];
 const func = async (msg: Eris.Message): Promise<Eris.Message> => {
 	const { activeUsers, cardCount, commandCount, cardsPerUser, commandsPerUser } = await stats.getMetrics();
+	const serverCount = bot.guilds.size;
+	const userCount = bot.users.size;
 	return await msg.channel.createMessage("I've calculated the following metrics:\n"
+    + "I am in **" + serverCount + " servers**.\n"
+    + "**" + userCount + " users** share a server with me.\n"
     + "**" + activeUsers + " unique users** have searched for cards and/or used commands.\n"
     + "**" + cardCount + " cards** (non-unique) have been searched for.\n"
     + "Each active user has searched an average of **" + cardsPerUser.toFixed(2) + " cards** each.\n"
