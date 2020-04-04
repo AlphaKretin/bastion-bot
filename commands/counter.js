4b3eee87cb4b92411b6d9a0bd16cf58a4bd23404
AlphaKretin <deewhydeeecks@gmail.com>
2020-04-05 02:09:04 +1030

    Update database source

--- commands/counter.js
+++ commands/counter.js
@@ -6,13 +6,13 @@ const util_1 = require("../modules/util");
 const names = ["counter"];
 const func = async (msg) => {
     const lang = util_1.getLang(msg);
-    let code = await ygopro_data_1.strings.reverseCounter(lang.msg, lang.lang1);
+    let code = await ygopro_data_1.counters.reverseCounter(lang.msg, lang.lang1);
     const tempCode = parseInt(lang.msg, 16);
     if (!isNaN(tempCode) && !code) {
         code = tempCode;
     }
     if (code) {
-        const set = await ygopro_data_1.strings.getCounter(code, lang.lang2);
+        const set = await ygopro_data_1.counters.getCounter(code, lang.lang2);
         if (set) {
             return await msg.channel.createMessage("`0x" + code.toString(16) + "`: " + set);
         }
