a8fecf4c408b2e9af33c34d33c6b4a2e7b0516b5
kevinlul <6320810+kevinlul@users.noreply.github.com>
2021-09-29 00:51:15 -0400

    Migrate .link to Slash Command

--- commands/link.ts
+++ commands/link.ts
@@ -1,24 +1,10 @@
 import { Message } from "eris";
 import { Command } from "../modules/Command";
-import { links } from "../config/botOpts.json";
-import { trimMsg } from "../modules/util";
 const names = ["link"];
 const func = async (msg: Message): Promise<Message> => {
-	const content = trimMsg(msg);
-	const query = content.toLowerCase().replace(/\s+/g, "");
-	const key = Object.keys(links).find(k => query.startsWith(k));
-	if (key) {
-		// cast links config to dictionary
-		return msg.channel.createMessage((links as { [key: string]: string })[key]);
-	} else {
-		return msg.channel.createMessage(
-			"Sorry, I don't have a link with that name. Try one of the following:\n`" + Object.keys(links).join("`, `") + "`"
-		);
-	}
+	return msg.channel.createMessage("This has been replaced by `/link`.");
 };
 
-const desc =
-	"Recalls one of a variety of useful links, such as common ruling resources.\n" +
-	"Call without a link name to see a list of valid names.";
+const desc = "Superseded by /link";
 
 export const command = new Command(names, func, undefined, desc, "name");
